970f101ce08ce96cb72cacdd2e92176621047ff9
unknown <kotwanisparsh@gmail.com>
2022-11-02 17:57:02 +0530

    rectified errors

--- Prime_Problem.js
+++ Prime_Problem.js
@@ -6,7 +6,7 @@ function prime(num)
         if(num%i==0)
         count++;
     }
-    if(count==1)
+    if(count==2)
      return true;
     else
      return false;
@@ -16,7 +16,7 @@ for(let i = 1; i<=N ;i++ )
 {
     let condition = prime(i);
     if(condition == true)
-    console.log(i,'Prime');
+    console.log(i,'is Prime');
     else
     console.log(i,'is not Prime');
 }
